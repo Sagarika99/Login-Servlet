d1e13b73a20db4b2a6452bde3d3e6d64104e3eec
Sagarika Shinde <sagarika.shinde@morningstar.com>
2021-09-21 17:49:24 +0530

    Created a simple Login with predefined user

---
+++ src/com/bridgelabz/LoginServlet.java
@@ -0,0 +1,56 @@
+package com.bridgelabz;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(
+		description = "Login Servlet Testing",
+		urlPatterns = { "/LoginServlet" },
+		initParams = {
+				@WebInitParam (name="user", value="Sagarika"),
+				@WebInitParam (name="password", value="Bridgelabz")
+		}
+)
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// get requests parameters for userid and password
+		String user = request.getParameter("user");
+		String pwd = request.getParameter("pwd");
+		
+		//get servlet config init params
+		String userID = getServletConfig().getInitParameter("user");
+		String password = getServletConfig().getInitParameter("password");
+		
+		if(userID.equals(user) && password.equals(pwd)) {
+			request.setAttribute("user", user);
+			request.getRequestDispatcher("LoginSuccess.jsp").forward(request, response);
+		}
+		else {
+			RequestDispatcher rd = getServletContext().getRequestDispatcher("/Login.html");
+			PrintWriter out = response.getWriter();
+			out.println("<font color=red> Either username or password is wrong</font>");
+			rd.include(request, response);
+		}
+	}
+
+}
